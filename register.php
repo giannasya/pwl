292f5ffb66209e2dd6e88cd681bd42949c064ab0
Gii <73086478+giannasya@users.noreply.github.com>
2023-01-13 01:59:33 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Login Form | CodingLab</title> -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  </head>
+  <body>
+    <div class="container">
+      <div class="wrapper">
+        <div class="title"><span>Register</span></div>
+        <form action="#">
+        <div class="row">
+            <i class="fab fa-google"></i>
+            <input type="text" placeholder="Gmail" required>
+          </div>
+          <div class="row">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Username" required>
+          </div>
+          <div class="row">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" required>
+          </div>
+          <div class="row button">
+            <input type="submit" value="Register">
+          </div>
+          <div class="signup-link">Have an account? <a href="Login.php">Login Here</a></div>
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
