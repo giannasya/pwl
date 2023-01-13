1a6c87ec33ea106c6b08359bb266453d0d16bf55
Gii <73086478+giannasya@users.noreply.github.com>
2023-01-13 09:57:29 +0700

    Add files via upload

---
+++ captcha.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+function acakCaptcha(){
+    $alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $pass = array();
+    $panjangAlpha = strlen($alphabet) - 2;
+    for ($i = 0; $i < 5; $i++){
+        $n = rand(0, $panjangAlpha);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass);
+}
+
+$code = acakCaptcha();
+$_SESSION["code"] = $code;
+$wh = imagecreatetruecolor(173, 50);
+$bgc = imagecolorallocate($wh, 22, 86, 165);
+$fc = imagecolorallocate($wh, 223, 230, 233);
+
+imagefill($wh, 0, 0, $bgc);
+imagestring($wh, 10, 50, 15, $code, $fc);
+
+header('content-type:image/jpg');
+imagejpeg($wh);
+imagedestroy($wh);
+
+?>
