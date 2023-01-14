81b421fbfe37790e4bcca89316b88fc95a69bb9d
Gii <73086478+giannasya@users.noreply.github.com>
2023-01-14 13:01:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  </head>
+  <style>
+    body{
+        background-image: url("landing.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        font-family: 'Poppins',sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+       
+        * {
+         margin:0; 
+         padding:0;
+        }
+    button{
+        font-family: sans-serif;
+        font-size: 15px;
+        background: #22a4cf;
+        color: white;
+        border: white 3px solid;
+        border-radius: 5px;
+        padding: 12px 20px;
+        margin-top: 10px;
+    }
+    
+
+    </style>
+</head>
+<body>
+<nav>
+     <ul>
+        <b>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Sign Up</a></li>
+            <li><a href="index.php">Home</a></li>
+        </b>
+     </ul>
+</nav>
+<marquee>Kami Siap Membantu Anda Dalam Melakukan Pengisian Surat Keterangan Untuk Mendapatkan Pembayaran Tunjangan Keluarga</marquee>
+<div class="judul">
+        <h1>SKUMPTK</h1>
+        <p><h5> Disamping gaji pokok, PNS juga di berikan beberapa tunjangan contohnya tunjangan keluarga, Tunjangan keluarga ini berupa tunjangan anak dan tunjangan istri/suami yang besarnya telah diatur sesuai dengan peraturan perundang-undangan yang berlaku.</h5></p>
+        <br>
+    </div>
+    <div class= "button">
+    <a href="login.php" type="submit" class="btn btn-info"> Login Disini</a>
+    </div>
+</body>
+</html>
